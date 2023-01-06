bb0802d971110cf091b036b6e9444af2b20c49f8
Skipper Strange <skipperstrange@gmail.com>
2023-01-06 08:43:32 +0000

    All home components ready

--- Views/partials/traits.php
+++ Views/partials/traits.php
@@ -4,14 +4,12 @@
     $bg_position = ((($index+2)%2)+1);
 ?>
 <div class="row">
-    <div class="col-lg-6 col-md-6 p-0 <?php if($bg_position === 2): echo "order-lg-2"; else: echo  "order-lg-1"; echo " style='background:url(".$traits[$index]['icon'].");'"; endif; ?>">
-        <section class="section m-0 border-0  appear-animation animated fadeInRightShorter appear-animation-visible" data-appear-animation="fadeInRightShorter" style="animation-delay: 100mshidden;padding:0;">
-        <div class="row m-0 mt-lg-5 pt-lg-0">
-                <div class="col-half-section mb-0 appear-animation animated fadeInRightShorter appear-animation-visible" data-appear-animation="fadeInRightShorter" data-appear-animation-delay="400" style="animation-delay: 400ms; overflow:hidden;padding:0;">
-                    
-                <!--img src="<?=$traits[$index]['icon']?>" alt="" srcset=""-->
-        </div>
-</div>
+    <div class="col-lg-6 col-md-6 p-0 <?php if($bg_position === 2): echo "order-lg-2"; else: echo  "order-lg-1"; endif; ?>">
+        <section class="section m-0 border-0  appear-animation animated fadeInRightShorter appear-animation-visible" data-appear-animation="fadeInRightShorter" 
+        style="animation-delay: 100mshidden;padding:0;background-image:url(<?=$value['icon']?>); 
+        height: 100%; background-repeat:no-repeat;
+        background-size:cover;">
+          
         </section>
     </div>
 
